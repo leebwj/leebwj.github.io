665dafd90e13321da842c8892aecf1300960583d
Brian Lee <109943383+leebwj@users.noreply.github.com>
2025-12-22 02:14:47 +0900

    Link Fix
    Minor fix and clean

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/* -----------------------------------------
-  Have focus outline only for keyboard users 
- ---------------------------------------- */
+/* Keyboard-only focus outlines */
 
 const handleFirstTab = (e) => {
   if(e.key === 'Tab') {
